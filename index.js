ceadbc30d56d61a14b8dc8d83f6f5cfc6eebd900
Ednah wairimu kimani <kimaniednah@gmail.com>
2025-09-22 17:29:07 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((object) => {
+      // append the id returned from the server to the DOM
+      document.body.innerHTML = object.id;
+    })
+    .catch((error) => {
+      // append the error message to the DOM
+      document.body.innerHTML = error.message;
+    });
+}
